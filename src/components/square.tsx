88cc3b931163266a2e0f166813d22f8ddbce7d4e
Yuhnosuk Hashimoto <h.yuunosuke@gmail.com>
2022-03-30 19:52:03 +0900

    store value in  state  at square component

--- src/components/square.tsx
+++ src/components/square.tsx
@@ -3,9 +3,28 @@ import React from 'react'
 interface Props {
   value: number
 }
-class Square extends React.Component<Props, {}> {
+
+interface State {
+  value: string | null
+}
+
+class Square extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>
+    return (
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
+    )
   }
 }
 
